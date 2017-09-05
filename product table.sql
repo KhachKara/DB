0ddba23b3296365833ecd353b0e47af72e8b5a18
khachkara@gmail.com <khachkara@gmail.com>
2017-09-05 17:27:27 +0300

    !

--- product table.sql
+++ product table.sql
@@ -10,3 +10,12 @@ insert into product (brend_id, produc_type_id, category_id, price) values (1, 1,
 insert into product (brend_id, produc_type_id, category_id, price) values (10, 1, 1, 8999);
 insert into product (brend_id, produc_type_id, category_id, price) values (2, 2, 2, 5000);
 insert into product (brend_id, produc_type_id, category_id, price) values (1, 3, 3, 6000);
+
+ALTER TABLE `new_shop`.`product` 
+ADD INDEX `fk_brend_product_idx` (`brend_id` ASC);
+ALTER TABLE `new_shop`.`product` 
+ADD CONSTRAINT `fk_brend_product`
+  FOREIGN KEY (`brend_id`)
+  REFERENCES `new_shop`.`brend` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
