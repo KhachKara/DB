d93cfa197d40da193f6e75e85c6611fe82fb4149
khachkara@gmail.com <khachkara@gmail.com>
2017-09-06 11:33:54 +0300

    Inner join

---
+++ Inner join.sql
@@ -0,0 +1,43 @@
+use shop;
+
+SELECT * FROM product;
+select * from category where id = 1 or id = 3 or id = 2;
+select * from category where id >= 1 and id <=3;
+select * from category where id IN (1, 2, 3);
+
+select * from product
+	inner join category on product.category_id = category.id;
+
+select product.id, price, name from product
+	inner join category on product.category_id = category.id; 	
+
+select * from category
+	 inner join product on product.category_id = category.id;
+
+select * from product
+	inner join category on product.category_id = category.id where discount <= 5;
+
+select * from product
+	inner join category on product.category_id = category.id where price < 10000;
+
+
+-- where price < 10000;
+
+select * from product
+  	inner join category on product.category_id = category.id
+	inner join brend on brend.id = product.brend_id
+ 	inner join product_type on product_type.id = product.produc_type_id;
+
+
+select category.name, category.discount, brend.name, price from product
+  	inner join category on product.category_id = category.id
+	inner join brend on brend.id = product.brend_id
+ 	inner join product_type on product_type.id = product.produc_type_id where price <= 9000;
+
+
+select brend.name, category.name, category.alias_name, category.discount, product_type.name, price from product
+	inner join product_type on product.produc_type_id = product_type.id
+    inner join brend on brend.id = product.brend_id
+    -- inner join product_type on product_type.id = product.produc_type_id;
+    inner join category on category_id = product.category_id
+    where product_type.name = 'Футболка';
