f12fbe3b14b9567ad8bd48a1e7d50846c4867fda
khachkara@gmail.com <khachkara@gmail.com>
2017-09-06 16:38:44 +0300

    Group by

---
+++ GROUP BY.sql
@@ -0,0 +1,29 @@
+use new_shop;
+
+insert into order_products (order_id, product_id, `count`) values (2, 8, 2);
+
+insert into order_products (order_id, product_id, `count`) values (2, 6, 3);
+SELECT * FROM new_shop.`order`;
+
+SELECT sum(price * `count`) as total_price from `order`
+ 
+	inner join order_products on order_products.order_id = `order`.id
+    
+	inner join product on product.id = order_products.product_id
+    where `order`.id = 2;
+    
+SELECT `order`.user_name, sum(price * `count`) as total_price  from `order` 
+	
+	inner join order_products on order_products.order_id = `order`.id
+    
+	inner join product on product.id = order_products.product_id
+    
+	group by `order`.user_name;
+    
+SELECT `order`.user_name, max(price), sum(`count`)  from `order` 
+	
+	inner join order_products on order_products.order_id = `order`.id
+    
+	inner join product on product.id = order_products.product_id
+    
+	group by `order`.user_name;
